e40b5fabc6173beb2ac504c7773d3a3b95efa667
irfan-hossain-bhuiyan <irfanhbhy@gmail.com>
2024-03-26 21:55:52 +0600

    1.Added some comment.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 pub mod basic_function;
+///It includes all the syntax tree,for the language,like expression and statememts.
 pub mod ast;
+///It has token data structure and Scanner that parse the token.
 pub mod token;
+///Create ast from token.
 pub mod parser;
+///run the ast
 pub mod interpreter;
+///This ia the value lox language support,Lox being a dynamic language,all types is actually one
+///type.
 pub mod lox_object;
+///This is for dispalying error.
 pub mod lox_error;
+///This is to run lox from a file.It 
+pub mod lox_runner;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{env, fs};
+
 fn main(){
- println!("It's working");
+    let args:Vec<String>=env::args().collect();
+    let path=match args.get(1){
+        Some(x)=>x,
+        None=>todo!(),
+    };
+    match fs::read_to_string(path) {
+        Some(x)=>lox_runner(x),
+        None=>todo!(),
+    }
+
 }
